47cc9e5eb8ce954df6ce5d91ca5c4abe265961a0
Tommymarvel <ibironketomiwa4@gmail.com>
2022-09-24 12:26:33 +0100

    Setup and features

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-//component file
-import TodoContainer from "./components/TodoContainer";
-ReactDOM.render(
+/* eslint-disable */
+import React from 'react';
+import './functionBased/App.css';
+import TodoContainer from './functionBased/components/TodoContainer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import * as ReactDOMClient from 'react-dom/client';
+
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <TodoContainer />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>
 );
+
+/* 
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+*/
